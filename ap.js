265c995af897cb044f7b25adf87b7b63a5246f2f
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-23 19:14:40 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,28 @@
+// marriage calculator
+var enter_Status = prompt('Enter you status,are you married,reply(yes/no)');
+if(enter_Status === 'no'){alert('I am agree to marry you')
+{var widower = prompt('are you widower(the man whose wife has died)reply yes/not')
+    if(widower === 'not'){alert('good, i am agree for marriage')}
+    else{alert('You are widower,i donot want to marry you,sorry')}
+    {var age = prompt ('Enter your age')
+        if(age >= 18 && age <= 30){alert('Good, you are eligible for marry me')}
+        else{alert('Sorry, i donot want to marry an old man')}
+        {var job = prompt('are you job holder reply yes/not')
+            if(job === 'yes'){alert('ok see my next condition')}
+            else{alert('I donot want to marry jobless man')}
+            {var car = prompt('Have you car,reply yes/not')
+                if(car ==='yes'){alert('ok see my next condition')}
+                else{'I donot want to marry man who has not car'}
+                {var model = prompt('Enter your car model like 2000,2025 etc')
+                    if(model >= 2020){alert('ok see my next condition')}
+                    else{alert('you have old model car, i donot want to marry you')}
+                }
+            }
+        }
+    }
+}
+}
+
+
+
+else{alert('I donot want to marry you')}
