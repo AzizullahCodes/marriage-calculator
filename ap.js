bfdcf564be854a203c85da3c9e56449401af387b
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-23 21:01:03 +0500

    u

--- ap.js
+++ ap.js
@@ -1,35 +1,48 @@
-// marriage calculator
-var enter_Status = prompt('Enter you status,are you married,reply(yes/no)');
-if(enter_Status === 'no'){alert('ok see my next conditon')
-{var widower = prompt('are you widower(the man whose wife has died)reply yes/not')
-    if(widower === 'not'){alert('ok see my next condition')}
-    else{alert('You are widower,i donot want to marry you,sorry')}
-    {var age = prompt ('Enter your age')
-        if(age >= 18 && age <= 30){alert('ok see my next condition')}
-        else if(age < 18){alert('sorry,you are children')}
-        else{alert('Sorry, you are old man')}
-        {var job = prompt('are you job holder reply yes/not')
-            if(job === 'yes'){alert('ok see my next condition')}
-            else{alert('I donot want to marry jobless man')}
-            {var car = prompt('Have you car,reply yes/not')
-                if(car ==='yes'){alert('ok see my next condition')}
-                else{'I donot want to marry man who has not car'}
-                {var model = prompt('Enter your car model like 2000,2025 etc')
-                    if(model >= 2020){alert('ok see my next condition')}
-                    else{alert('you have old model car, i donot want to marry you')}
-                    {var home = prompt('Do you have own home (yes/not)')
-                        if(home === 'yes'){alert('ok see my next condition')}
-                        else{alert('I donot want to live in rent home so i donot want to marry you,you are poor')}
-                        {var combine_Family = prompt('Have you combine family system(yes/not)')
-                            if(combine_Family === 'not'){alert('ok see my next condition')}
-                            else{alert('I donot marry you because i donot like combine family system')}
-                            { var second_Marriage = prompt('will you second marry after marriage me(yes/not)')
-                                if(second_Marriage === 'not'){alert('ok see my next condition')}
-                                else{alert('you are deceiver i donot want to marry you')}
-                                {var honey_Moon = prompt('Where will we celebrate honey moon(abroad/pakistan)')
-                                    if(honey_Moon === 'abroad'){alert('ok see my next condition')}
-                                    else{alert('I want to celebrate honey moon abroad so i donot want to marry you')}
-                                    alert('I will think about marry you,after thinking I will reply you, you have passed interview but I will see you in marit list')
+// Marriage calculator use only lowecase
+var enter_Status = prompt('Enter your status. Are you married? (yes/not)');
+if (enter_Status !== 'not') {
+    alert('I do not want to marry you.');
+} else {
+    var widower = prompt('Are you a widower (man whose wife has died)? (yes/not)');
+    if (widower !== 'not') {
+        alert('You are a widower, I do not want to marry you. Sorry!');
+    } else {
+        var age = parseInt(prompt('Enter your age'));
+        if (age < 18) {
+            alert('Sorry, you are a child.');
+        } else if (age > 30) {
+            alert('Sorry, you are too old.');
+        } else {
+            var job = prompt('Are you a job holder? (yes/not)');
+            if (job !== 'yes') {
+                alert('I do not want to marry a jobless man.');
+            } else {
+                var car = prompt('Do you have a car? (yes/not)');
+                if (car !== 'yes') {
+                    alert('I do not want to marry a man who has no car.');
+                } else {
+                    var model = parseInt(prompt('Enter your car model (e.g. 2000, 2025)'));
+                    if (model < 2020) {
+                        alert('Your car is old model, I do not want to marry you.');
+                    } else {
+                        var home = prompt('Do you own a home? (yes/not)');
+                        if (home !== 'yes') {
+                            alert('I do not want to live in a rented home. You are poor.');
+                        } else {
+                            var combine_Family = prompt('Do you live in a combined family system? (yes/not)');
+                            if (combine_Family !== 'not') {
+                                alert('I do not want to marry you because I dislike combined family system.');
+                            } else {
+                                var second_Marriage = prompt('Will you do a second marriage after marrying me? (yes/not)');
+                                if (second_Marriage !== 'not') {
+                                    alert('You are a deceiver. I do not want to marry you.');
+                                } else {
+                                    var honey_Moon = prompt('Where will we celebrate honeymoon? (abroad/pakistan)');
+                                    if (honey_Moon !== 'abroad') {
+                                        alert('I want to celebrate honeymoon abroad. So, I do not want to marry you.');
+                                    } else {
+                                        alert('You passed the interview. I will think about marrying you and let you know after checking the merit list!');
+                                    }
                                 }
                             }
                         }
@@ -39,8 +52,3 @@ if(enter_Status === 'no'){alert('ok see my next conditon')
         }
     }
 }
-}
-
-
-
-else{alert('I donot want to marry you')}
